ef8f6878097ce2fd5d8c1fae583a0c34421cb796
Maksim Borovik <mouxez@gmail.com>
2022-05-14 23:32:43 +0200

    add card component & common type

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,15 @@
+import { FC, ReactNode } from "react";
+import styles from "./Card.module.css";
+
+const { container } = styles;
+
+type CardProps = {
+  children: ReactNode;
+  className?: string;
+};
+
+const Card: FC<CardProps> = ({ children, className }) => {
+  return <div className={`${container} ${className}`}>{children}</div>;
+};
+
+export default Card;
